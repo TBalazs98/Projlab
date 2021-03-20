984582dfd84977ac00bb557dc5fc1e91c429a110
Ualk4 <65612065+Ualk4@users.noreply.github.com>
2021-03-20 20:04:13 +0100

    material (nincs kész)

--- src/Model/Material.java
+++ src/Model/Material.java
@@ -4,25 +4,21 @@ import java.util.*;
 
 public class Material  {
 
-
-
     NormalMaterialName name;
-    public Material() {
-        name = NormalMaterialName.IRON;
+    public Material() { }
+
+    public void Add(Inventory inventory) {
+        inventory.Add(this);
     }
 
-
-
-    public void Add(HashMap list) {
-        // TODO implement here
-    }
-
-    public void Hit(Asteroid a) {
-        // TODO implement here
-    }
+    public void Hit(Asteroid a) { }
 
     public void Remove(HashMap list) {
-        // TODO implement here
+
+    }
+
+    public void setName(NormalMaterialName n){
+        name = n;
     }
 
 }
