437a778f68581a526fcc594a2658001a0972ee2a
Dávid Nagy <nagymail2@gmail.com>
2021-03-22 02:16:06 +0100

    Main komik törölve

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -5,45 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-//        Material m = new Material();
-//        m.setName(NormalMaterialName.COAL);,
-//        RadioactiveMaterial m = new RadioactiveMaterial();
-//        m.setName(RadioactiveMaterialName.URAN);
-//        Asteroid a = new Asteroid();
-//        a.AddMaterial(m);
-//
-//        Robot r = new Robot();
-//        r.setAsteroid(a);
-//        a.Accept(r);
-//
-//        Settler s = new Settler();
-//        s.setAsteroid(a);
-//        a.Accept(s);
-//
-//        Asteroid b = new Asteroid();
-//        TeleportGate tg = new TeleportGate();
-//        TeleportGate pair = new TeleportGate();
-//
-//        tg.setAsteroid(a);
-//        pair.setAsteroid(b);
-//
-//        s.setAsteroid(a);
-//        tg.setPair(pair);
-//        pair.setPair(tg);
-//        a.setCharacter(s);
-//        a.setNeighbour(tg);
-//        b.setNeighbour(pair);
-
-        //s.Move(0);
-
-//        s.Mine();
-//
-//        s.PlaceMaterial(m);
-
         Test t = new Test();
-        t.menu();               //teszt osztály
+        t.menu();               //teszt megvalositasa
+
     }
 
 
