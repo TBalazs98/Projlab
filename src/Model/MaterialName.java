5f582368028fd2c9b0c6caf40ed88f3bc189eda7
Dávid Nagy <nagymail2@gmail.com>
2021-03-22 12:58:32 +0100

    komi dework by David

--- src/Model/MaterialName.java
+++ src/Model/MaterialName.java
@@ -3,6 +3,7 @@ package Model;
 
 /**
  * A nyersagat neveit megtestesito interfesz.
+ * https://stackoverflow.com/questions/34675961/generalize-3-enums-into-one-type
  */
 public interface MaterialName {
 
