3c8a36619a38c04c530533e3f4041036a7219d3c
Bence Bihari <bencebihari@edu.bme.hu>
2021-03-27 17:18:07 +0100

    determinisztikussag otlet

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,4 +1,7 @@
 package Model;
+
+import java.util.Random;
+
 /**
  * @author Bakonyi Klaudia
  * @author Bihari Bence
@@ -6,12 +9,17 @@ package Model;
  * @author Toth Balazs
  * @author Wagner Reka
  */
+
 public class Main {
     /**
      * Jelenleg a Test nevu tesztosztalyunk segitsegevel ellenorizzuk modellunk helyesseget.
      */
     public static void main(String[] args) {
-
+/*
+        boolean isDeterministicRun = fajlbeolvasas; // "a veletlenszeruseg ki-bekapcsolhato legyen"
+        if (!isDeterministicRun){
+        Random rand = new Random(System.currentTimeMillis()); //random szam az ido fuggvenyeben, tehat a random szamok statikusak lesznek a lefutasok kozott
+    }*/
         Test t = new Test();
         t.menu();               //teszt menu meghivasa
 
