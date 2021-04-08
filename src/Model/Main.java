f3e6fd8d554ccfbc4f089947934c1f7d4af168a8
Dávid Nagy <nagymail2@gmail.com>
2021-04-08 19:52:12 +0200

    bena regex, de mukodik, david2

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -3,6 +3,7 @@ package Model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
@@ -37,8 +38,10 @@ public class Main {
 
         int[] cuccosok = {0,0,0,0,0,0}; //Material, Asteroid, TG, Settler, Robot, Ufo
 
+        Material[] material;
+
         int sum =0;
-        //System.out.println(Pattern.matches("\\W*((?i)createmap(?-i))\\W*\\s(\\d+\\s){5}\\d+","createmap 1 2 32321 0 33123 5433"));
+        //System.out.println(Pattern.matches("\\W*((?i)createmap(?-i))\\W*\\s(\\d+\\s){5}\\d+","createmap 1 2 3 4 5 6"));
         System.out.println(Pattern.matches(regex,input));
         if(Pattern.matches(regex,input)){
             String[] cmd=input.split(" ");
@@ -47,11 +50,29 @@ public class Main {
                 cuccosok[i-1]=Integer.parseInt(cmd[i]);
                 sum+=cuccosok[i-1];
             }
+
+            System.out.println(sum);
+            for(int i=0; i<cuccosok.length;i++){
+                System.out.println(cuccosok[i]);
+            }
+
         }
-        System.out.println(sum);
-        for(int i=0; i<cuccosok.length;i++){
-            System.out.println(cuccosok[i]);
+
+        ArrayList<Material> materials = new ArrayList<>();
+        for (int i = 0; i< cuccosok[0];i++){
+            materials.add(new Material());
         }
+
+        for(int i = 0; i< materials.size();i++){
+            System.out.println(materials.get(i));
+        }
+
+
+
+
+
+
+
 
 
 
