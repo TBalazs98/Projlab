cc5f0abc572eeb2a6ba8b2235139b36b266384fa
Dávid Nagy <nagymail2@gmail.com>
2021-04-16 18:04:44 +0200

    Asteroid regex kinda jó, createasteroid félig done - D

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -29,8 +29,28 @@ public class Main {
 
     String regex = "\\W*((?i)createmap(?-i))\\W*\\s(\\d+\\s){5}\\d+";
     static String materialregex = "[013]|2\\s[012]";
-    static String asteroidregex ="0\\s\\d\\s\\d\\s0|\\d\\s(\\d,)*\\d\\s[01]\\s\\d{1,2}\\s1\\s\\d\n";
-    static String asteroidregex2 ="0\\s\\d\\s\\d\\s0|\\d\\s(\\d,)*\\d\\s[01]\\s\\d{1,2}\\s1\\s\\d ||\\d\\s(\\d,)*\\d\\s[01]\\s\\d{1,2}";
+
+    static String lonely_empty_asteroid ="0\\s[01]\\s\\d{1,}\\s0";
+    //input : szomszédszám = [kötelezően 0], napközel = [0,1], rétegszám = [bármi int], üresség = [kötelezően 0]
+    //pl    : 0 0 25 0
+
+    static String lonely_not_empty_asteroid ="0\\s[01]\\s\\d{1,}\\s1\\s\\d{1,}";
+    //input : szomszédszám = [kötelezően 0], napközel = [0,1], rétegszám = [bármi int], üresség = [kötelezően 1], nyersi index = [bármi int]
+    //pl    : 0 0 25 1 5
+
+    static String not_lonely_not_empty_asteroid ="\\d\\s(\\d{1,},{0,1})*\\s[01]\\s\\d{1,}\\s1\\s\\d{1,}";
+    //input : szomszédszám = [bármi int], szomszéd index[bármi int, (utolsó után nem kell)],  napközel = [0,1], rétegszám = [bármi int], üresség = [kötelezően 1], nyersi index = [bármi int]
+    //pl    : 2 5,2 1 5 1 1
+    //hint  : egyelőre nem nézi a vesszőket a szomszédok között
+    //        egyelőre nem nézi hogy annyi szomszéd index legyen beolvasva, mint amennyit megadtunk az első inputban
+    static String not_lonely_empty="\\d\\s(\\d{1,},{0,1})*\\s[01]\\s\\d{1,}\\s0";
+    //input : szomszédszám = [bármi int], szomszéd index[bármi int, (utolsó után nem kell)],  napközel = [0,1], rétegszám = [bármi int], üresség = [kötelezően 0]
+    //pl    : 5 5,2,3,4,5 1 5 0
+    //hint  : egyelőre nem nézi a vesszőket a szomszédok között
+    //        egyelőre nem nézi hogy annyi szomszéd index legyen beolvasva, mint amennyit megadtunk az első inputban
+
+    //itt tudtok regexel baszkodni : https://regex101.com/
+
     String tgregex ="\\d\\s[01]\\s\\d\\s[01]\\s[01]";
     String settlerregex ="\\d\\s\\d\\s(\\d,)*\\d\\s[0123]\\s\\d|\\d\\s0\\s0";
     String robotregex ="\\d";
@@ -69,7 +89,8 @@ public class Main {
             }
 
             CommandManager cm = new CommandManager();
-            cm.listMaterials();
+            //cm.listMaterials();
+            cm.listAsteroids();
 
 //
 //            reader.close();
@@ -83,9 +104,10 @@ public class Main {
 
 
 
-        for(int i=0; i<Main.materials.size();i++){
-            System.out.println(Main.materials.get(i).name);
-        }
+//        for(int i=0; i<Main.materials.size();i++){
+//            System.out.println(Main.materials.get(i).name);
+//        }
+
 
 
     }
@@ -99,10 +121,15 @@ public class Main {
                     break;
                     }
             case 1:
-                if(Pattern.matches(asteroidregex,params)){
+                if(Pattern.matches(lonely_empty_asteroid,params)){
                     createAsteroid1(params);
-                }else if(Pattern.matches(asteroidregex2,params))
+                }else if(Pattern.matches(lonely_not_empty_asteroid,params)) {
                     createAsteroid2(params);
+                }else if(Pattern.matches(not_lonely_empty,params)){
+                    createAsteroid3(params);
+                }else if(Pattern.matches(not_lonely_not_empty_asteroid,params)){
+                    createAsteroid4(params);
+                }
             case 2:
             case 3:
             case 4:
@@ -146,11 +173,36 @@ public class Main {
         }
     }
 
-    public static void createAsteroid1(String params){
+    public static void createAsteroid1(String params){        //input : szomszédszám = [kötelezően 0], napközel = [0,1], rétegszám = [bármi int], üresség = [kötelezően 0]
+                                                              //pl    : 0 0 25 0
+        String[] cmd = params.split(" ");
+        Main.asteroids.add(new Asteroid());
+        Asteroid a =Main.asteroids.get(Main.asteroids.size()-1);
+        Main.setCommonAsteroid(a,Integer.parseInt(cmd[1]),Integer.parseInt(cmd[2]),Integer.parseInt(cmd[3]));
 
     }
-    public static void createAsteroid2(String params){
+    public static void createAsteroid2(String params){         //input : szomszédszám = [kötelezően 0], napközel = [0,1], rétegszám = [bármi int], üresség = [kötelezően 1], nyersi index = [bármi int]
+                                                                //pl    : 0 0 25 1 5
+        String[] cmd = params.split(" ");
+        Main.asteroids.add(new Asteroid());
+        Asteroid a =Main.asteroids.get(Main.asteroids.size()-1);
+        Main.setCommonAsteroid(a,Integer.parseInt(cmd[1]),Integer.parseInt(cmd[2]),Integer.parseInt(cmd[3]));
+        a.AddMaterial(Main.materials.get(Integer.parseInt(cmd[4])-1));
 
+    }
+    public static void createAsteroid3(String params){         //input : szomszédszám = [bármi int], szomszéd index[bármi int, (utolsó után nem kell)],  napközel = [0,1], rétegszám = [bármi int], üresség = [kötelezően 1], nyersi index = [bármi int]
+                                                               //pl    : 2 5,2 1 5 1 1
+
+    }
+    public static void createAsteroid4(String params){          //input : szomszédszám = [bármi int], szomszéd index[bármi int, (utolsó után nem kell)],  napközel = [0,1], rétegszám = [bármi int], üresség = [kötelezően 0]
+                                                                 //pl    : 5 5,2,3,4,5 1 5 0
+
+    }
+
+    public static void setCommonAsteroid(Asteroid a, int sunprox, int layer, int empty){
+        //a.setSunProximity
+        a.setLayer(layer);
+        //a.setEmpty
     }
 
 
