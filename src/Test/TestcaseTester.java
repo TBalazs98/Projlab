ad8bc08197a3f36d481133f1bc01eaf137660d10
Bence Bihari <bencebihari@edu.bme.hu>
2021-04-18 14:08:46 +0200

    tester init

---
+++ src/Test/TestcaseTester.java
@@ -0,0 +1,34 @@
+import Controller.InputManager;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestcaseTester {
+
+    @Test
+    public void Test1() {
+
+
+
+    }
+    @Test
+    public void Test2() {
+
+
+
+    }
+    @Test
+    public void Test3() {
+
+    }
+    @Test
+    public void Test4() {
+
+    }
+    @Test
+    public void Test5() {
+
+    }
+    @Test
+    public void Test6() {
+
+    }
+}
