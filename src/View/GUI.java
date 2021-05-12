cf99161d71d55a0c4c397b0a1d35c176e906c9a3
Dávid Nagy <nagymail2@gmail.com>
2021-05-12 11:59:55 +0200

    gui komii

--- src/View/GUI.java
+++ src/View/GUI.java
@@ -18,25 +18,29 @@ public class GUI extends JFrame implements ActionListener {
     MenuBar bar = new MenuBar(this);
     public JLayeredPane gamespace= new JLayeredPane();
     public DetailsPanel dp;
-    public CommandPanel cp ;/*= new CommandPanel(this);*/
+    public CommandPanel cp ;
     public CustomGamePanel custompanel;
     public LoadPanel loadpanel;
-    public boolean isopened = false;
+
 
     private ArrayList<IDrawable> drawables;
     public  ArrayList<SettlerView> settlers=new ArrayList<>();
     private ArrayList<AsteroidView> asteroids = new ArrayList<>();
     private ArrayList<RobotView> robots = new ArrayList<>();
     public  ArrayList<UfoView> ufos=new ArrayList<>();
-    public ArrayList<TeleportGateView> teleportgates = new ArrayList<>();    //#todo
+    public ArrayList<TeleportGateView> teleportgates = new ArrayList<>();
     public  ArrayList<MaterialView> materials=new ArrayList<>();
     private Coordinates[][] coords;
     private final int coordswidth;
     private final int coordsheight;
     private Random gen = new Random();
     private ArrayList<Model.Character> chars = new ArrayList<>();
-    public JPanel inpudialog = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
+
+    /**
+     * konstruktor
+     * @param _c
+     */
     public GUI(Controller _c) {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("AsteroidMine");
@@ -66,10 +70,11 @@ public class GUI extends JFrame implements ActionListener {
             }
         }
 
-        System.out.println(this.width + " x " + this.height + "\t" + coordswidth + " x " + coordsheight);
-
     }
 
+    /**
+     * Minden koordinatat szabadda teszunk
+     */
     public void clearCoords()
     {
         for(int i=0; i<coordswidth;i++)
@@ -77,16 +82,11 @@ public class GUI extends JFrame implements ActionListener {
                 coords[j][i].setToggled(false);
     }
 
-    private Coordinates getRandEmptyCoord() {
-        int x = gen.nextInt(coordsheight);
-        int y = gen.nextInt(coordswidth);
-        while(coords[x][y].isToggled()) {
-            x = gen.nextInt(coordsheight);
-            y = gen.nextInt(coordswidth);
-        }
-        return coords[x][y];
-    }
 
+    /**
+     * Getter egy olyan kordinatara ami nem foglalt
+     * @return pozicio ahova lehet rajzolni
+     */
     private Coordinates getLonelyCoord() {
         int x = gen.nextInt(coordsheight);
         int y = gen.nextInt(coordswidth);
@@ -99,23 +99,19 @@ public class GUI extends JFrame implements ActionListener {
     }
 
 
-
+    /**
+     * Kezdeti inicializalo metodus
+     */
     public void DrawAll(){
-
         this.setLayout(new FlowLayout());
-
         gamespace.removeAll();
         gamespace.setPreferredSize(new Dimension(width,height-250));
-
         dp = new DetailsPanel();
-
         cp = new CommandPanel();
-
         JPanel controls = new JPanel();
         controls.setBackground(new Color(0,0,0,64));
         controls.setLayout(new FlowLayout());
         gamespace.setLayout(null);
-
         this.add(gamespace, BorderLayout.CENTER);
 
 
@@ -133,8 +129,6 @@ public class GUI extends JFrame implements ActionListener {
 
         for(int i=0; i<this.settlers.size();i++){
             settlers.get(i).SetCoords(getAsteroidViewByAsteroid( settlers.get(i).getSettler().getAsteroid()).getX(),getAsteroidViewByAsteroid( settlers.get(i).getSettler().getAsteroid()).getY());
-            System.out.println("x=" +getAsteroidViewByAsteroid( settlers.get(i).getSettler().getAsteroid()).getX() + "y=" +getAsteroidViewByAsteroid( settlers.get(i).getSettler().getAsteroid()).getY());
-//            gamespace.add(settlers.get(i).l);
             gamespace.add(settlers.get(i).l,1);
             settlers.get(i).Draw();
         }
@@ -151,75 +145,98 @@ public class GUI extends JFrame implements ActionListener {
 
     }
 
+    /**
+     * Kirajzolja a settlereket
+     */
     public void DrawSettlers(){
         for(int i=0; i<this.settlers.size();i++){
             if(settlers.get(i)!=null) {
                 settlers.get(i).SetCoords(getAsteroidViewByAsteroid(settlers.get(i).getSettler().getAsteroid()).getX(), getAsteroidViewByAsteroid(settlers.get(i).getSettler().getAsteroid()).getY());
-                System.out.println("x=" + getAsteroidViewByAsteroid(settlers.get(i).getSettler().getAsteroid()).getX() + "y=" + getAsteroidViewByAsteroid(settlers.get(i).getSettler().getAsteroid()).getY());
                 settlers.get(i).Draw();
             }
         }
     }
 
+    /**
+     * Megkeres egy aszteroida View-t egy aszteroida alapjan
+     * @param a asteroida
+     * @return Asteroid View
+     */
     public AsteroidView getAsteroidViewByAsteroid(Asteroid a){
         for (int i=0; i<this.asteroids.size();i++){
             if(this.asteroids.get(i)!=null) {
                 if (this.asteroids.get(i).getAsteroid() == a) {
-                    // System.out.println("fasz");
                     return this.asteroids.get(i);
                 }
             }
         }
         return null;
     }
-
+    /**
+     * Megkeres egy settler View-t egy settler alapjan
+     * @param s settler
+     * @return settler View
+     */
     public SettlerView getSettlerViewBySettler(Settler s){
         for (int i=0; i<settlers.size();i++){
             if(settlers.get(i).getSettler()==s){
-                //System.out.println("fasz");
                 return settlers.get(i);
             }
 
         }
         return null;
     }
-
+    /**
+     * Megkeres egy TG View-t egy TG alapjan
+     * @param tg asteroida
+     * @return TG View
+     */
     public TeleportGateView getTeleportGateViewByTeleportGate(TeleportGate tg){
         for (int i=0; i<teleportgates.size();i++){
             if(teleportgates.get(i).getTg()==tg){
-                //System.out.println("fasz");
                 return teleportgates.get(i);
             }
 
         }
         return null;
     }
-
+    /**
+     * Megkeres egy UFO View-t egy UFO alapjan
+     * @param u asteroida
+     * @return UFO View
+     */
     public UfoView getUfoViewByUfo(UFO u){
         for (int i=0; i<ufos.size();i++){
             if(ufos.get(i).getUfo()==u){
-                //System.out.println("fasz");
                 return ufos.get(i);
             }
 
         }
         return null;
     }
-
+    /**
+     * Megkeres egy MAterial View-t egy Material alapjan
+     * @param m asteroida
+     * @return Material View
+     */
     public MaterialView getMaterialViewByMaterial(Material m){
         for (int i=0; i<materials.size();i++){
             if(materials.get(i).getMaterial()==m){
-                //System.out.println("fasz");
                 return materials.get(i);
             }
 
         }
         return null;
     }
+
+    /**
+     * Megkeres egy Robot View-t egy Tobot alapjan
+     * @param r asteroida
+     * @return Robot View
+     */
     public RobotView getRobotViewByRobot(Model.Robot r){
         for (int i=0; i<robots.size();i++){
             if(robots.get(i).getRobot()==r){
-                //System.out.println("fasz");
                 return robots.get(i);
             }
 
@@ -227,7 +244,9 @@ public class GUI extends JFrame implements ActionListener {
         return null;
     }
 
-
+    /**
+     * load panel megjelenitese
+     */
     public void Load(){
         FlowLayout layout = new FlowLayout();
         layout.setVgap(this.height/12);
@@ -239,6 +258,9 @@ public class GUI extends JFrame implements ActionListener {
 
     }
 
+    /**
+     * settings panel megjelenitese
+     */
     public void Settings() {
         FlowLayout layout = new FlowLayout();
         layout.setVgap(this.height/12);
@@ -251,8 +273,10 @@ public class GUI extends JFrame implements ActionListener {
         this.setVisible(true);
     }
 
+    /**
+     * megjeleniti a menut
+     */
     public void  DrawMenu(){
-
         this.repaint();
         this.validate();
         this.setLayout(new FlowLayout());
@@ -265,9 +289,7 @@ public class GUI extends JFrame implements ActionListener {
         SetPanel(title);
         this.add(title);
 
-        /**TODO
-         * SZAR AZ ELÉRÉSI ÚT:3
-         */
+
         sg = new JPanel();
         startgame = new JButton( new ImageIcon("Files/Pictures/startbtn.png"));
         startgame.setRolloverIcon(new ImageIcon("Files/Pictures/startdarkbtn.png"));
@@ -334,40 +356,66 @@ public class GUI extends JFrame implements ActionListener {
             System.exit(0);
     }
 
+    /**
+     * letrehoz és hozzaad a settler view tombhoz egyet egy settler apajan
+     * @param s settler
+     */
     public void addSettler(Settler s ){
         SettlerView sw = new SettlerView(s);
         settlers.add(sw);
         drawables.add(sw);
     }
-
+    /**
+     * letrehoz és hozzaad a aszteroidat view tombhoz egyet egy aszteroida apajan
+     * @param a aszteroida
+     */
     public void addAsteroid(Asteroid a ){
         AsteroidView av = new AsteroidView(a);
         asteroids.add(av);
         drawables.add(av);
     }
+    /**
+     * letrehoz és hozzaad a robot view tombhoz egyet egy tobot apajan
+     * @param r robot
+     */
     public void addRobot(Model.Robot r ){
         RobotView rv = new RobotView(r);
         robots.add(rv);
         drawables.add(rv);
     }
-
+    /**
+     * letrehoz és hozzaad a ufo view tombhoz egyet egy ufo apajan
+     * @param u settler
+     */
     public void addUfo(UFO u ){
         UfoView uv = new UfoView(u);
         ufos.add(uv);
         drawables.add(uv);
     }
+    /**
+     * letrehoz és hozzaad a tg view tombhoz egyet egy tg apajan
+     * @param tg tg
+     */
     public void addTeleportGate(TeleportGate tg ){       //#todo teleportgate view
         TeleportGateView tgv = new TeleportGateView(tg);
         teleportgates.add(tgv);
         drawables.add(tgv);
     }
 
+    /**
+     * letrehoz és hozzaad a material view tombhoz egyet egy material apajan
+     * @param m material
+     */
     public void addMaterial(Material m ){
         MaterialView mv = new MaterialView(m);
         materials.add(mv);
         drawables.add(mv);
     }
 
+    /**
+     * a gombok egyseges megjelenitesere
+     * @param button gomb
+     */
     public void SetButton(JButton button) {
         button.setBorderPainted(false);
         button.setContentAreaFilled(false);
@@ -376,17 +424,33 @@ public class GUI extends JFrame implements ActionListener {
         button.addActionListener(this);
     }
 
+    /**
+     * ablak szelessege
+     * @return szeleseeg
+     */
     public int GetWidth(){
         return this.width;
     }
+
+    /**
+     * visszaadja a settlerview tombot
+     * @return settlerview
+     */
     public  ArrayList<SettlerView> GetSettlerView(){return settlers;}
+    /**
+     * visszaadja a aszteroid tombot
+     * @return aszteroid
+     */
     public  ArrayList<AsteroidView> GetAsteroidView(){return asteroids;}
     public void SetPanel(JPanel panel) {
         panel.setOpaque(false);
     }
 
+    /**
+     * updateli az aszteroidakat es ezaltal meg sokmindent
+     */
     public void update() {
-        //gamespace.removeAll();
+
         for(AsteroidView av: asteroids) {
                 if (av.getAsteroid() != null) {
                     av.Draw();
